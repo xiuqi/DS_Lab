16b54e3ed75adee1dc29d0aac0f34ab4431e0a96
xiuqi <wsc91129@gmail.com>
2014-02-11 13:20:10 -0500

    Revert "lab2"
    This reverts commit 9b78988fda5e0269f7a68bf350d3d0ad346632ae.

--- all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-./logger.sh config.txt logger vector & 
-./run.sh config.txt alice vector &
-./run.sh config.txt bob vector &
-./run.sh config.txt charlie vector & 
-./run.sh config.txt daphnie vector &
